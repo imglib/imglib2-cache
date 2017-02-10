2cc7eabc4978e45868124cf47b9af65a3922bd50
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2017-02-09 19:26:27 -0500

    SoftRef based implementations of Cache and ListenableCache

---
+++ src/main/java/net/imglib2/cache/ref/SoftRefCache.java
@@ -0,0 +1,141 @@
+package net.imglib2.cache.ref;
+
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.SoftReference;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+
+import net.imglib2.cache.Cache;
+import net.imglib2.cache.CacheLoader;
+
+public class SoftRefCache< K, V > implements Cache< K, V >
+{
+	final ConcurrentHashMap< K, Entry > map = new ConcurrentHashMap<>();
+
+	final ReferenceQueue< V > queue = new ReferenceQueue<>();
+
+	final class CacheSoftReference extends SoftReference< V >
+	{
+		private final Entry entry;
+
+		public CacheSoftReference()
+		{
+			super( null );
+			this.entry = null;
+		}
+
+		public CacheSoftReference( final V referent, final Entry entry )
+		{
+			super( referent, queue );
+			this.entry = entry;
+		}
+
+		public void clean()
+		{
+			map.remove( entry.key, entry );
+		}
+	}
+
+	final class Entry
+	{
+		final K key;
+
+		private SoftReference< V > ref;
+
+		boolean loaded;
+
+		public Entry( final K key )
+		{
+			this.key = key;
+			this.ref = new CacheSoftReference();
+			this.loaded = false;
+		}
+
+		public V getValue()
+		{
+			return ref.get();
+		}
+
+		public void setValue( final V value )
+		{
+			this.loaded = true;
+			this.ref = new CacheSoftReference( value, this );
+		}
+	}
+
+	@Override
+	public V getIfPresent( final K key )
+	{
+		cleanUp();
+		final Entry entry = map.get( key );
+		return entry == null ? null : entry.getValue();
+	}
+
+	@Override
+	public V get( final K key, final CacheLoader< ? super K, ? extends V > loader ) throws ExecutionException
+	{
+		cleanUp();
+		final Entry entry = map.computeIfAbsent( key, ( k ) -> new Entry( k ) );
+		V value = entry.getValue();
+		if ( value == null )
+		{
+			synchronized ( entry )
+			{
+				if ( entry.loaded )
+				{
+					value = entry.getValue();
+					if ( value == null )
+					{
+						/*
+						 * The entry was already loaded, but its value has been
+						 * garbage collected. We need to create a new entry
+						 */
+						map.remove( key, entry );
+						value = get( key, loader );
+					}
+				}
+				else
+				{
+					try
+					{
+						value = loader.get( key );
+						entry.setValue( value );
+					}
+					catch ( final InterruptedException e )
+					{
+						Thread.currentThread().interrupt();
+						throw new ExecutionException( e );
+					}
+					catch ( final Exception e )
+					{
+						throw new ExecutionException( e );
+					}
+				}
+			}
+		}
+		return value;
+	}
+
+	@Override
+	public void invalidateAll()
+	{
+		// TODO
+		throw new UnsupportedOperationException( "not implemented yet" );
+	}
+
+	/**
+	 * Remove entries from the cache whose references have been
+	 * garbage-collected.
+	 */
+	public void cleanUp()
+	{
+		while ( true )
+		{
+			@SuppressWarnings( "unchecked" )
+			final CacheSoftReference poll = ( CacheSoftReference ) queue.poll();
+			if ( poll == null )
+				break;
+			poll.clean();
+		}
+	}
+}
---
+++ src/main/java/net/imglib2/cache/ref/SoftRefListenableCache.java
@@ -0,0 +1,205 @@
+package net.imglib2.cache.ref;
+
+import java.lang.ref.PhantomReference;
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.SoftReference;
+import java.lang.reflect.Field;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+
+import net.imglib2.cache.CacheLoader;
+import net.imglib2.cache.ListenableCache;
+import net.imglib2.cache.RemovalListener;
+
+/**
+ * TODO: Consider running periodically calling {@link #processRemovalQueue()}
+ * from a background thread. Otherwise, freeing memory depends on the cache
+ * being regularly used. (This is different for PhantomRefs than for
+ * Weak/SoftRefs. PhantomRefs must be explicitly clear()ed before the referent
+ * is freed.)
+ *
+ * @param <K>
+ * @param <V>
+ *
+ * @author Tobias Pietzsch &lt;tobias.pietzsch@gmail.com&gt;
+ */
+public class SoftRefListenableCache< K, V > implements ListenableCache< K, V >
+{
+	final ConcurrentHashMap< K, Entry > map = new ConcurrentHashMap<>();
+
+	final ReferenceQueue< V > queue = new ReferenceQueue<>();
+
+	static final class CachePhantomReference< V > extends PhantomReference< V >
+	{
+		static Field referent = null;
+		{
+			try
+			{
+				referent = Reference.class.getDeclaredField( "referent" );
+			}
+			catch ( NoSuchFieldException | SecurityException e )
+			{
+				e.printStackTrace();
+			}
+			referent.setAccessible( true );
+		}
+
+		SoftRefListenableCache< ?, V >.Entry entry;
+
+		public CachePhantomReference( final V referent, final ReferenceQueue< V > remove, final SoftRefListenableCache< ?, V >.Entry entry )
+		{
+			super( referent, remove );
+			this.entry = entry;
+		}
+
+		@SuppressWarnings( "unchecked" )
+		public V resurrect()
+		{
+			try
+			{
+				return ( V ) referent.get( this );
+			}
+			catch ( IllegalArgumentException | IllegalAccessException e )
+			{
+				e.printStackTrace();
+				return null;
+			}
+		}
+	}
+
+	final class Entry
+	{
+		final K key;
+
+		private SoftReference< V > ref;
+
+		private CachePhantomReference< V > phantomRef;
+
+		private RemovalListener< ? super K, ? super V > remover;
+
+		boolean loaded;
+
+		public Entry( final K key )
+		{
+			this.key = key;
+			this.ref = new SoftReference<>( null );
+			this.phantomRef = null;
+			this.remover = null;
+			this.loaded = false;
+		}
+
+		public V getValue()
+		{
+			return ref.get();
+		}
+
+		public void setValue( final V value )
+		{
+			this.loaded = true;
+			this.ref = new SoftReference<>( value );
+		}
+
+		public void setValue( final V value, final RemovalListener< ? super K, ? super V > remover )
+		{
+			this.loaded = true;
+			this.ref = new SoftReference<>( value );
+			this.phantomRef = new CachePhantomReference<>( value, queue, this );
+			this.remover = remover;
+		}
+
+		public synchronized void remove()
+		{
+			if ( remover != null )
+			{
+				final V value = phantomRef.resurrect();
+				phantomRef.clear();
+				phantomRef = null;
+				remover.onRemoval( key, value );
+				remover = null;
+				map.remove( key, this );
+			}
+		}
+	}
+
+	@Override
+	public V getIfPresent( final K key )
+	{
+		cleanUp();
+		final Entry entry = map.get( key );
+		return entry == null ? null : entry.getValue();
+	}
+
+	@Override
+	public V get( final K key, final CacheLoader< ? super K, ? extends V > loader, final RemovalListener< ? super K, ? super V > remover ) throws ExecutionException
+	{
+		cleanUp();
+		final Entry entry = map.computeIfAbsent( key, ( k ) -> new Entry( k ) );
+		V value = entry.getValue();
+		if ( value == null )
+		{
+			synchronized ( entry )
+			{
+				if ( entry.loaded )
+				{
+					value = entry.getValue();
+					if ( value == null )
+					{
+						/*
+						 * The entry was already loaded, but its value has been
+						 * garbage collected. We need to create a new entry
+						 */
+						entry.remove();
+					}
+				}
+				else
+				{
+					try
+					{
+						value = loader.get( key );
+						entry.setValue( value, remover );
+					}
+					catch ( final InterruptedException e )
+					{
+						Thread.currentThread().interrupt();
+						throw new ExecutionException( e );
+					}
+					catch ( final Exception e )
+					{
+						throw new ExecutionException( e );
+					}
+				}
+			}
+		}
+
+		if ( value == null )
+			value = get( key, loader, remover );
+
+		return value;
+	}
+
+	@Override
+	public void invalidateAll()
+	{
+		// TODO
+		throw new UnsupportedOperationException( "not implemented yet" );
+	}
+
+	/**
+	 * Remove entries from the cache whose references have been
+	 * garbage-collected. The {@link RemovalListener} (specified in
+	 * {@link #get(Object, CacheLoader, RemovalListener)}), is notified for each
+	 * removed entry.
+	 */
+	public void cleanUp()
+	{
+		while ( true )
+		{
+			@SuppressWarnings( "unchecked" )
+			final CachePhantomReference< V > pr = ( CachePhantomReference< V > ) queue.poll();
+			if ( pr == null )
+				break;
+			pr.entry.remove();
+		}
+	}
+}
