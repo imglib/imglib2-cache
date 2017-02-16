3bbaf08c6dbe439a1d96da5eb6dff4046a1c5491
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2017-02-16 11:32:34 -0500

    javadoc

--- src/main/java/net/imglib2/cache/util/KeyBimap.java
+++ src/main/java/net/imglib2/cache/util/KeyBimap.java
@@ -2,6 +2,18 @@ package net.imglib2.cache.util;
 
 import java.util.function.Function;
 
+/**
+ * A bidirectional map between key types {@code S} and {@code T} (source and
+ * target). This is used to create adapters exposing a {@code T→V} cache as
+ * a {@code S→V} cache.
+ *
+ * @param <S>
+ *            source type
+ * @param <T>
+ *            target type
+ *
+ * @author Tobias Pietzsch &lt;tobias.pietzsch@gmail.com&gt;
+ */
 public interface KeyBimap< S, T >
 {
 	T getTarget( S source );
