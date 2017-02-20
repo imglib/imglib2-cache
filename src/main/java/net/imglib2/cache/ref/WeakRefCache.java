dee659559f09bab88e1972c9ed64d53007ca8247
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2017-02-19 20:25:30 -0500

    WeakRef based implementations of Cache
    This is basically a copy of SoftRefCache.

---
+++ src/main/java/net/imglib2/cache/ref/WeakRefCache.java
@@ -0,0 +1,141 @@
+package net.imglib2.cache.ref;
+
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+
+import net.imglib2.cache.Cache;
+import net.imglib2.cache.CacheLoader;
+
+public class WeakRefCache< K, V > implements Cache< K, V >
+{
+	final ConcurrentHashMap< K, Entry > map = new ConcurrentHashMap<>();
+
+	final ReferenceQueue< V > queue = new ReferenceQueue<>();
+
+	final class CacheWeakReference extends WeakReference< V >
+	{
+		private final Entry entry;
+
+		public CacheWeakReference()
+		{
+			super( null );
+			this.entry = null;
+		}
+
+		public CacheWeakReference( final V referent, final Entry entry )
+		{
+			super( referent, queue );
+			this.entry = entry;
+		}
+
+		public void clean()
+		{
+			map.remove( entry.key, entry );
+		}
+	}
+
+	final class Entry
+	{
+		final K key;
+
+		private CacheWeakReference ref;
+
+		boolean loaded;
+
+		public Entry( final K key )
+		{
+			this.key = key;
+			this.ref = new CacheWeakReference();
+			this.loaded = false;
+		}
+
+		public V getValue()
+		{
+			return ref.get();
+		}
+
+		public void setValue( final V value )
+		{
+			this.loaded = true;
+			this.ref = new CacheWeakReference( value, this );
+		}
+	}
+
+	@Override
+	public V getIfPresent( final K key )
+	{
+		cleanUp();
+		final Entry entry = map.get( key );
+		return entry == null ? null : entry.getValue();
+	}
+
+	@Override
+	public V get( final K key, final CacheLoader< ? super K, ? extends V > loader ) throws ExecutionException
+	{
+		cleanUp();
+		final Entry entry = map.computeIfAbsent( key, ( k ) -> new Entry( k ) );
+		V value = entry.getValue();
+		if ( value == null )
+		{
+			synchronized ( entry )
+			{
+				if ( entry.loaded )
+				{
+					value = entry.getValue();
+					if ( value == null )
+					{
+						/*
+						 * The entry was already loaded, but its value has been
+						 * garbage collected. We need to create a new entry
+						 */
+						map.remove( key, entry );
+						value = get( key, loader );
+					}
+				}
+				else
+				{
+					try
+					{
+						value = loader.get( key );
+						entry.setValue( value );
+					}
+					catch ( final InterruptedException e )
+					{
+						Thread.currentThread().interrupt();
+						throw new ExecutionException( e );
+					}
+					catch ( final Exception e )
+					{
+						throw new ExecutionException( e );
+					}
+				}
+			}
+		}
+		return value;
+	}
+
+	@Override
+	public void invalidateAll()
+	{
+		// TODO
+		throw new UnsupportedOperationException( "not implemented yet" );
+	}
+
+	/**
+	 * Remove entries from the cache whose references have been
+	 * garbage-collected.
+	 */
+	public void cleanUp()
+	{
+		while ( true )
+		{
+			@SuppressWarnings( "unchecked" )
+			final CacheWeakReference poll = ( CacheWeakReference ) queue.poll();
+			if ( poll == null )
+				break;
+			poll.clean();
+		}
+	}
+}
