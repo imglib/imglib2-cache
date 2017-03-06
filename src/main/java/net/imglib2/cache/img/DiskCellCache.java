35d805a3e859654d1cc10290db97b3418ff15b72
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2017-03-05 22:11:22 -0500

    A basic disk cache implementing RemovalListener/CacheLoader
    Blocks which are not in the disk cache yet are obtained from a backing
    CacheLoader. (Which could for example provide empty new cells in the case
    of creating a giant empty disk cached CellImg).

---
+++ src/main/java/net/imglib2/cache/img/DiskCellCache.java
@@ -0,0 +1,197 @@
+package net.imglib2.cache.img;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel.MapMode;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+
+import net.imglib2.cache.CacheLoader;
+import net.imglib2.cache.IoSync;
+import net.imglib2.cache.RemovalListener;
+import net.imglib2.img.cell.Cell;
+import net.imglib2.img.cell.CellGrid;
+import net.imglib2.type.NativeType;
+import net.imglib2.util.Fraction;
+import net.imglib2.util.Intervals;
+
+/**
+ * Basic {@link RemovalListener}/{@link CacheLoader} for writing/reading cells
+ * to a disk cache. Currently blocks are simply written as flat files to a
+ * specified directory. {@link #createTempDirectory(String, boolean)} can be
+ * used to create a temporary directory that will be automatically removed when
+ * the JVM shuts down.
+ * <p>
+ * Blocks which are not in the diskcache (yet) are obtained from a backing
+ * {@link CacheLoader}.
+ * </p>
+ * <p><em>
+ * A {@link DiskCellCache} should be connected to a in-memory cache through
+ * {@link IoSync} if the cache will be used concurrently by multiple threads!
+ * </em></p>
+ *
+ * @param <A>
+ *            access type
+ *
+ * @author Tobias Pietzsch
+ */
+public class DiskCellCache< A > implements RemovalListener< Long, Cell< A > >, CacheLoader< Long, Cell< A > >
+{
+	private final Path blockcache;
+
+	private final CellGrid grid;
+
+	private final int n;
+
+	private final Fraction entitiesPerPixel;
+
+	private final AccessIo< A > accessIo;
+
+	private final CacheLoader< Long, Cell< A > > backingLoader;
+
+	public < T extends NativeType< T > > DiskCellCache(
+			final Path blockcache,
+			final CellGrid grid,
+			final CacheLoader< Long, Cell< A > > backingLoader,
+			final AccessIo< A > accessIo,
+			final Fraction entitiesPerPixel )
+	{
+		this.blockcache = blockcache;
+		this.grid = grid;
+		this.n = grid.numDimensions();
+		this.entitiesPerPixel = entitiesPerPixel;
+		this.accessIo = accessIo;
+		this.backingLoader = backingLoader;
+	}
+
+	private String blockname( final long index )
+	{
+//		final long[] cellGridPosition = new long[ n ];
+//		grid.getCellGridPositionFlat( index, cellGridPosition );
+
+
+		return String.format( "%s/%d", blockcache, index );
+	}
+
+	@Override
+	public Cell< A > get( final Long key ) throws Exception
+	{
+		final long index = key;
+		final String filename = blockname( index );
+
+		if ( new File( filename ).exists() )
+		{
+			final long[] cellMin = new long[ n ];
+			final int[] cellDims = new int[ n ];
+			grid.getCellDimensions( index, cellMin, cellDims );
+			final long numEntities = entitiesPerPixel.mulCeil( Intervals.numElements( cellDims ) );
+			final long bytesize = numEntities * accessIo.getBytesPerElement();
+			try (
+					final RandomAccessFile mmFile = new RandomAccessFile( filename, "r" ); )
+			{
+				final MappedByteBuffer in = mmFile.getChannel().map( MapMode.READ_ONLY, 0, bytesize );
+				final A access = accessIo.load( in, ( int ) numEntities );
+				return new Cell<>( cellDims, cellMin, access );
+			}
+		}
+		else
+		{
+			return backingLoader.get( key );
+		}
+	}
+
+	@Override
+	public void onRemoval( final Long key, final Cell< A > value )
+	{
+		final long index = key;
+		final String filename = blockname( index );
+
+		final int[] cellDims = new int[ n ];
+		value.dimensions( cellDims );
+		final long blocksize = entitiesPerPixel.mulCeil( Intervals.numElements( cellDims ) );
+		final long bytesize = blocksize * accessIo.getBytesPerElement();
+		try (
+				final RandomAccessFile mmFile = new RandomAccessFile( filename, "rw" ); )
+		{
+			final MappedByteBuffer out = mmFile.getChannel().map( MapMode.READ_WRITE, 0, bytesize );
+			accessIo.save( value.getData(), out, ( int ) blocksize );
+		}
+		catch ( final IOException e )
+		{
+			throw new RuntimeException( e );
+		}
+	}
+
+	// Adapted from http://stackoverflow.com/a/20280989
+	static class DeleteTempFilesHook extends Thread
+	{
+		private final ArrayList< Path > tempPaths = new ArrayList<>();
+
+		public void add( final Path path )
+		{
+			tempPaths.add( path );
+		}
+
+		@Override
+		public void run()
+		{
+			for ( final Path path : tempPaths )
+			{
+				try
+				{
+					Files.walkFileTree( path, new SimpleFileVisitor< Path >()
+					{
+						@Override
+						public FileVisitResult visitFile( final Path file, final BasicFileAttributes attrs ) throws IOException
+						{
+							Files.delete( file );
+							return FileVisitResult.CONTINUE;
+						}
+
+						@Override
+						public FileVisitResult postVisitDirectory( final Path dir, final IOException e ) throws IOException
+						{
+							if ( e == null )
+							{
+								Files.delete( dir );
+								return FileVisitResult.CONTINUE;
+							}
+							// directory iteration failed
+							throw e;
+						}
+					} );
+				}
+				catch ( final IOException e )
+				{
+					throw new RuntimeException( e );
+				}
+			}
+		}
+	}
+
+	static DeleteTempFilesHook deleteTempFilesHook = null;
+
+	public static Path createTempDirectory( final String prefix, final boolean deleteOnExit ) throws IOException
+	{
+		final Path tmp = Files.createTempDirectory( prefix );
+		System.out.println( tmp );
+
+		if ( deleteOnExit )
+		{
+			if ( deleteTempFilesHook == null )
+			{
+				deleteTempFilesHook = new DeleteTempFilesHook();
+				Runtime.getRuntime().addShutdownHook( deleteTempFilesHook );
+			}
+			deleteTempFilesHook.add( tmp );
+		}
+
+		return tmp;
+	}
+}
