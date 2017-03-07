0e583ae40ae99c539eb1180e4700cfafb1a86ced
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2017-03-07 12:46:35 -0500

    Add ListenableCache backed by Caffeine and WeakRefs
    This allows to have size-limited caches in cases where SoftRefs are too
    optimistic.
    
    The surface to Caffeine cache is very small, only getIfPresent() and put()
    are used. This should make it very easy to offer Guava etc as alternative
    options. I decided for Caffeine now, because it is favored by benchmarks I
    could find, and also because it is a smaller dependency.

---
+++ src/main/java/net/imglib2/cache/ref/GuardedStrongRefListenableCache.java
@@ -0,0 +1,237 @@
+package net.imglib2.cache.ref;
+
+import java.lang.ref.PhantomReference;
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
+import java.lang.reflect.Field;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+
+import net.imglib2.cache.CacheLoader;
+import net.imglib2.cache.ListenableCache;
+import net.imglib2.cache.RemovalListener;
+
+/**
+ * A {@link ListenableCache} that is backed by a cache with strong references to
+ * values. At the moment the backing cache is we use is
+ * <a href="https://github.com/ben-manes/caffeine">caffeine</a>. We can easily
+ * add Guava, cache2k, etc options later.
+ * <p>
+ * In addition we keep weak and phantom references to values. This ensures that
+ * we never lose track of values that are still in use elsewhere, although they
+ * have been evicted from the backing cache. Thus we never end up in a situation
+ * where two distinct values are associated to the same key (associated
+ * conceptually by the application, not by the cache map).
+ * </p>
+ * <p>
+ * TODO: Consider running periodically calling {@link #processRemovalQueue()}
+ * from a background thread. Otherwise, freeing memory depends on the cache
+ * being regularly used. (This is different for PhantomRefs than for
+ * Weak/SoftRefs. PhantomRefs must be explicitly clear()ed before the referent
+ * is freed.)
+ * </p>
+ *
+ * @param <K>
+ *            key type
+ * @param <V>
+ *            value type
+ *
+ * @author Tobias Pietzsch
+ */
+public class GuardedStrongRefListenableCache< K, V > implements ListenableCache< K, V >
+{
+	final ConcurrentHashMap< K, Entry > map = new ConcurrentHashMap<>();
+
+	final ReferenceQueue< V > queue = new ReferenceQueue<>();
+
+	final Cache< K, V > strongCache;
+
+	static final class CachePhantomReference< V > extends PhantomReference< V >
+	{
+		static Field referent = null;
+		static {
+			try
+			{
+				referent = Reference.class.getDeclaredField( "referent" );
+			}
+			catch ( NoSuchFieldException | SecurityException e )
+			{
+				e.printStackTrace();
+			}
+			referent.setAccessible( true );
+		}
+
+		GuardedStrongRefListenableCache< ?, V >.Entry entry;
+
+		public CachePhantomReference( final V referent, final ReferenceQueue< V > remove, final GuardedStrongRefListenableCache< ?, V >.Entry entry )
+		{
+			super( referent, remove );
+			this.entry = entry;
+		}
+
+		@SuppressWarnings( "unchecked" )
+		public V resurrect()
+		{
+			try
+			{
+				return ( V ) referent.get( this );
+			}
+			catch ( IllegalArgumentException | IllegalAccessException e )
+			{
+				e.printStackTrace();
+				return null;
+			}
+		}
+	}
+
+	final class Entry
+	{
+		final K key;
+
+		private WeakReference< V > ref;
+
+		private CachePhantomReference< V > phantomRef;
+
+		private RemovalListener< ? super K, ? super V > remover;
+
+		boolean loaded;
+
+		public Entry( final K key )
+		{
+			this.key = key;
+			this.ref = new WeakReference<>( null );
+			this.phantomRef = null;
+			this.remover = null;
+			this.loaded = false;
+		}
+
+		public V getValue()
+		{
+			return ref.get();
+		}
+
+		public void setValue( final V value )
+		{
+			this.loaded = true;
+			this.ref = new WeakReference<>( value );
+		}
+
+		public void setValue( final V value, final RemovalListener< ? super K, ? super V > remover )
+		{
+			this.loaded = true;
+			this.ref = new WeakReference<>( value );
+			this.phantomRef = new CachePhantomReference<>( value, queue, this );
+			this.remover = remover;
+		}
+
+		public synchronized void remove()
+		{
+			if ( remover != null )
+			{
+				final V value = phantomRef.resurrect();
+				phantomRef.clear();
+				phantomRef = null;
+				remover.onRemoval( key, value );
+				remover = null;
+				map.remove( key, this );
+			}
+		}
+	}
+
+	public GuardedStrongRefListenableCache( final long maximumSize )
+	{
+		strongCache = Caffeine.newBuilder().maximumSize( maximumSize ).build();
+	}
+
+	@Override
+	public V getIfPresent( final K key )
+	{
+		cleanUp();
+		final V value = strongCache.getIfPresent( key );
+		if ( value != null )
+			return value;
+		final Entry entry = map.get( key );
+		return entry == null ? null : entry.getValue();
+	}
+
+	@Override
+	public V get( final K key, final CacheLoader< ? super K, ? extends V > loader, final RemovalListener< ? super K, ? super V > remover ) throws ExecutionException
+	{
+		cleanUp();
+		V value = strongCache.getIfPresent( key );
+		if ( value != null )
+			return value;
+		final Entry entry = map.computeIfAbsent( key, ( k ) -> new Entry( k ) );
+		value = entry.getValue();
+		if ( value == null )
+		{
+			synchronized ( entry )
+			{
+				if ( entry.loaded )
+				{
+					value = entry.getValue();
+					if ( value == null )
+					{
+						/*
+						 * The entry was already loaded, but its value has been
+						 * garbage collected. We need to create a new entry
+						 */
+						entry.remove();
+					}
+				}
+				else
+				{
+					try
+					{
+						value = loader.get( key );
+						entry.setValue( value, remover );
+						strongCache.put( key, value );
+					}
+					catch ( final InterruptedException e )
+					{
+						Thread.currentThread().interrupt();
+						throw new ExecutionException( e );
+					}
+					catch ( final Exception e )
+					{
+						throw new ExecutionException( e );
+					}
+				}
+			}
+		}
+
+		if ( value == null )
+			value = get( key, loader, remover );
+
+		return value;
+	}
+
+	@Override
+	public void invalidateAll()
+	{
+		// TODO
+		throw new UnsupportedOperationException( "not implemented yet" );
+	}
+
+	/**
+	 * Remove entries from the cache whose references have been
+	 * garbage-collected. The {@link RemovalListener} (specified in
+	 * {@link #get(Object, CacheLoader, RemovalListener)}), is notified for each
+	 * removed entry.
+	 */
+	public void cleanUp()
+	{
+		while ( true )
+		{
+			@SuppressWarnings( "unchecked" )
+			final CachePhantomReference< V > pr = ( CachePhantomReference< V > ) queue.poll();
+			if ( pr == null )
+				break;
+			pr.entry.remove();
+		}
+	}
+}
