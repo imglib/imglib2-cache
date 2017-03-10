ca7604852a4ece82ba3657ac0b10081b1f625dea
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2017-03-10 17:04:47 -0500

    Add default utility methods

--- src/main/java/net/imglib2/cache/volatiles/VolatileCache.java
+++ src/main/java/net/imglib2/cache/volatiles/VolatileCache.java
@@ -2,7 +2,14 @@ package net.imglib2.cache.volatiles;
 
 import java.util.concurrent.ExecutionException;
 
+import net.imglib2.cache.util.VolatileCacheAsVolatileLoadingCacheAdapter;
+
 public interface VolatileCache< K, V > extends AbstractVolatileCache< K, V >
 {
 	V get( K key, VolatileCacheLoader< ? super K, ? extends V > loader, CacheHints cacheHints ) throws ExecutionException;
+
+	public default VolatileLoadingCache< K, V > withLoader( final VolatileCacheLoader< K, V > loader )
+	{
+		return new VolatileCacheAsVolatileLoadingCacheAdapter<>( this, loader );
+	}
 }
--- src/main/java/net/imglib2/cache/volatiles/VolatileLoadingCache.java
+++ src/main/java/net/imglib2/cache/volatiles/VolatileLoadingCache.java
@@ -2,7 +2,14 @@ package net.imglib2.cache.volatiles;
 
 import java.util.concurrent.ExecutionException;
 
+import net.imglib2.cache.util.VolatileLoadingCacheAsUncheckedVolatileLoadingCacheAdapter;
+
 public interface VolatileLoadingCache< K, V > extends AbstractVolatileCache< K, V >
 {
 	V get( K key, CacheHints cacheHints ) throws ExecutionException;
+
+	public default UncheckedVolatileLoadingCache< K, V > unchecked()
+	{
+		return new VolatileLoadingCacheAsUncheckedVolatileLoadingCacheAdapter<>( this );
+	}
 }
