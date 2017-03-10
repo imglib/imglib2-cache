1ffb99e8b3181236d12f3d9e4bca4ff8be633fe5
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2017-03-10 17:23:32 -0500

    Rename forgotten interfaces.

--- src/main/java/net/imglib2/cache/volatiles/VolatileLoaderRemoverCache.java
+++ src/main/java/net/imglib2/cache/volatiles/VolatileLoaderRemoverCache.java
@@ -4,7 +4,7 @@ import java.util.concurrent.ExecutionException;
 
 import net.imglib2.cache.CacheRemover;
 
-public interface VolatileListenableCache< K, V > extends AbstractVolatileCache< K, V >
+public interface VolatileLoaderRemoverCache< K, V > extends AbstractVolatileCache< K, V >
 {
 	V get( K key, VolatileCacheLoader< ? super K, ? extends V > loader, CacheRemover< ? super K, ? super V > remover, CacheHints cacheHints ) throws ExecutionException;
 }
--- src/main/java/net/imglib2/cache/volatiles/VolatileRemoverCache.java
+++ src/main/java/net/imglib2/cache/volatiles/VolatileRemoverCache.java
@@ -4,7 +4,7 @@ import java.util.concurrent.ExecutionException;
 
 import net.imglib2.cache.CacheRemover;
 
-public interface VolatileListenableLoadingCache< K, V > extends AbstractVolatileCache< K, V >
+public interface VolatileRemoverCache< K, V > extends AbstractVolatileCache< K, V >
 {
 	V get( K key, CacheRemover< ? super K, ? super V > remover, CacheHints cacheHints ) throws ExecutionException;
 }
