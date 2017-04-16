4406f2188c7f2d7a6f1849b49f79622bfd529baf
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2017-04-16 16:47:19 -0400

    Add FunctorLoader
    This is very handy for trivial image loaders, such as gradients or other image features.

---
+++ src/main/java/net/imglib2/cache/util/FunctorLoader.java
@@ -0,0 +1,44 @@
+package net.imglib2.cache.util;
+
+import java.util.stream.IntStream;
+
+import net.imglib2.FinalInterval;
+import net.imglib2.Interval;
+import net.imglib2.cache.CacheLoader;
+import net.imglib2.img.cell.Cell;
+import net.imglib2.img.cell.CellGrid;
+
+public class FunctorLoader< A > implements CacheLoader< Long, Cell< A > >
+{
+
+	public interface Functor< T, R > {
+
+		public R call( T t ) throws Exception;
+
+	}
+
+	private final CellGrid grid;
+
+	private final Functor< Interval, A > functor;
+
+	public FunctorLoader( final CellGrid grid, final Functor< Interval, A > functor )
+	{
+		this.grid = grid;
+		this.functor = functor;
+	}
+
+	@Override
+	public Cell< A > get( final Long key ) throws Exception
+	{
+		final long index = key;
+
+		final int n = grid.numDimensions();
+		final long[] cellMin = new long[ n ];
+		final int[] cellDims = new int[ n ];
+		grid.getCellDimensions( index, cellMin, cellDims );
+		final long[] cellMax = IntStream.range( 0, n ).mapToLong( d -> cellMin[ d ] + cellDims[ d ] - 1 ).toArray();
+		final A result = functor.call( new FinalInterval( cellMin, cellMax ) );
+		return new Cell<>( cellDims, cellMin, result );
+	}
+
+}
