06cbd7609ca93240dce3bc8158254f9b889e9238
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2017-04-18 10:47:12 -0400

    Use Functino instead of CacheLoader to generate store
    CellImg doesn't deal with exception so we follow suit and do not throw in IntervalKeyLoaderAsLongKeyLoader.get

--- src/main/java/net/imglib2/cache/util/IntervalKeyLoaderAsLongKeyLoader.java
+++ src/main/java/net/imglib2/cache/util/IntervalKeyLoaderAsLongKeyLoader.java
@@ -33,6 +33,7 @@
  */
 package net.imglib2.cache.util;
 
+import java.util.function.Function;
 import java.util.stream.IntStream;
 
 import net.imglib2.FinalInterval;
@@ -66,16 +67,16 @@ public class IntervalKeyLoaderAsLongKeyLoader< A > implements CacheLoader< Long,
 
 	private final CellGrid grid;
 
-	private final CacheLoader< Interval, A > intervalKeyLoader;
+	private final Function< Interval, A > intervalKeyLoader;
 
-	public IntervalKeyLoaderAsLongKeyLoader( final CellGrid grid, final CacheLoader< Interval, A > intervalKeyLoader )
+	public IntervalKeyLoaderAsLongKeyLoader( final CellGrid grid, final Function< Interval, A > intervalKeyLoader )
 	{
 		this.grid = grid;
 		this.intervalKeyLoader = intervalKeyLoader;
 	}
 
 	@Override
-	public Cell< A > get( final Long key ) throws Exception
+	public Cell< A > get( final Long key )
 	{
 		final long index = key;
 
@@ -84,7 +85,7 @@ public class IntervalKeyLoaderAsLongKeyLoader< A > implements CacheLoader< Long,
 		final int[] cellDims = new int[ n ];
 		grid.getCellDimensions( index, cellMin, cellDims );
 		final long[] cellMax = IntStream.range( 0, n ).mapToLong( d -> cellMin[ d ] + cellDims[ d ] - 1 ).toArray();
-		final A result = intervalKeyLoader.get( new FinalInterval( cellMin, cellMax ) );
+		final A result = intervalKeyLoader.apply( new FinalInterval( cellMin, cellMax ) );
 		return new Cell<>( cellDims, cellMin, result );
 	}
 
