27b69fa3cfdc571850e0fde1aae683d620ae764f
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2019-01-21 18:57:31 +0100

    Fix inheritance hierarchy: UncheckedVolatileLoaderCache extends AbstractUncheckedVolatileCache

--- src/main/java/net/imglib2/cache/volatiles/UncheckedVolatileLoaderCache.java
+++ src/main/java/net/imglib2/cache/volatiles/UncheckedVolatileLoaderCache.java
@@ -1,6 +1,6 @@
 package net.imglib2.cache.volatiles;
 
-public interface UncheckedVolatileLoaderCache< K, V > extends AbstractVolatileCache< K, V >
+public interface UncheckedVolatileLoaderCache< K, V > extends AbstractUncheckedVolatileCache< K, V >
 {
 	V get( K key, VolatileCacheLoader< ? super K, ? extends V > loader, CacheHints cacheHints );
 }
