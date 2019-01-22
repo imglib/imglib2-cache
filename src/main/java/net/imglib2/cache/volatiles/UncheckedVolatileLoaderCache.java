1c2d71136b0251be6e06d2c41de65a6a5f7c326b
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2019-01-22 16:51:17 +0100

    add TODO comments

--- src/main/java/net/imglib2/cache/volatiles/UncheckedVolatileLoaderCache.java
+++ src/main/java/net/imglib2/cache/volatiles/UncheckedVolatileLoaderCache.java
@@ -1,5 +1,11 @@
 package net.imglib2.cache.volatiles;
 
+/**
+ * This is only here for consistency with the non-volatile cache interfaces.
+ * There is no implementation at the moment.
+ * <p>
+ * TODO: REMOVE?
+ */
 public interface UncheckedVolatileLoaderCache< K, V > extends AbstractUncheckedVolatileCache< K, V >
 {
 	V get( K key, VolatileCacheLoader< ? super K, ? extends V > loader, CacheHints cacheHints );
--- src/main/java/net/imglib2/cache/volatiles/VolatileRemoverCache.java
+++ src/main/java/net/imglib2/cache/volatiles/VolatileRemoverCache.java
@@ -4,6 +4,12 @@ import java.util.concurrent.ExecutionException;
 
 import net.imglib2.cache.CacheRemover;
 
+/**
+ * This is only here for consistency with the non-volatile cache interfaces.
+ * There is no implementation at the moment.
+ * <p>
+ * TODO: REMOVE?
+ */
 public interface VolatileRemoverCache< K, V > extends AbstractVolatileCache< K, V >
 {
 	V get( K key, CacheRemover< ? super K, ? super V > remover, CacheHints cacheHints ) throws ExecutionException;
