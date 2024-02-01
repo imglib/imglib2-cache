c760374a826c31ef9a6d64f388c858ba220281d4
tpietzsch <tobias.pietzsch@gmail.com>
2024-02-01 14:13:44 +0100

    WIP: Use CellGrid.CellDimensionsAndSteps

--- src/main/java/net/imglib2/cache/util/IntervalKeyLoaderAsLongKeyLoader.java
+++ src/main/java/net/imglib2/cache/util/IntervalKeyLoaderAsLongKeyLoader.java
@@ -33,8 +33,8 @@
  */
 package net.imglib2.cache.util;
 
+import java.util.Arrays;
 import java.util.function.Function;
-import java.util.stream.IntStream;
 
 import net.imglib2.FinalInterval;
 import net.imglib2.Interval;
@@ -79,10 +79,10 @@ public class IntervalKeyLoaderAsLongKeyLoader< A > implements CacheLoader< Long,
 
 		final int n = grid.numDimensions();
 		final long[] cellMin = new long[ n ];
-		final int[] cellDims = new int[ n ];
-		grid.getCellDimensions( index, cellMin, cellDims );
-		final long[] cellMax = IntStream.range( 0, n ).mapToLong( d -> cellMin[ d ] + cellDims[ d ] - 1 ).toArray();
-		final A result = intervalKeyLoader.apply( new FinalInterval( cellMin, cellMax ) );
-		return new Cell<>( cellDims, cellMin, result );
+		final long[] cellMax = new long[ n ];
+		final CellGrid.CellDimensionsAndSteps dimsAndSteps = grid.getCellDimensions( index, cellMin );
+		Arrays.setAll( cellMax, d -> cellMin[ d ] + dimsAndSteps.dimensions()[ d ] - 1 );
+		final A result = intervalKeyLoader.apply( FinalInterval.wrap( cellMin, cellMax ) );
+		return new Cell<>( dimsAndSteps, cellMin, result );
 	}
 }
