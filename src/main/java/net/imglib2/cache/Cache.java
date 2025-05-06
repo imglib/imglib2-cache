eacea17f048e6a4225cbaef9254d030d8105dc9c
tpietzsch <tobias.pietzsch@gmail.com>
2025-05-06 20:50:58 +0200

    Remove redundant modifiers

--- src/main/java/net/imglib2/cache/Cache.java
+++ src/main/java/net/imglib2/cache/Cache.java
@@ -44,12 +44,12 @@ public interface Cache< K, V > extends AbstractCache< K, V >, CacheLoader< K, V 
 	@Override
 	V get( K key ) throws ExecutionException;
 
-	public default UncheckedCache< K, V > unchecked()
+	default UncheckedCache< K, V > unchecked()
 	{
 		return new CacheAsUncheckedCacheAdapter<>( this );
 	}
 
-	public default < T > Cache< T, V > mapKeys( final KeyBimap< T, K > keymap )
+	default < T > Cache< T, V > mapKeys(final KeyBimap<T, K> keymap)
 	{
 		return new CacheKeyAdapter<>( this, keymap );
 	}
